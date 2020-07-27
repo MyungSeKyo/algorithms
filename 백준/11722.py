8401a5646bf453eaefc8f0eb03494e407c21c2aa
myungsegyo <myungsekyo@gmail.com>
2020-07-27 14:43:12 +0900

    11722

---
+++ 백준/11722.py
@@ -0,0 +1,16 @@
+import sys
+import collections
+input = sys.stdin.readline
+
+n = int(input())
+arr = list(map(int, input().split()))
+dp = [0] * n
+
+for i in range(n):
+    maxi = 0
+    for j in range(i):
+        if arr[j] > arr[i]:
+            maxi = max(maxi, dp[j])
+    dp[i] = maxi + 1
+
+print(max(dp))
